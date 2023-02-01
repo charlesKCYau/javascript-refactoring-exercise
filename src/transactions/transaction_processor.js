b2aeeb140371e9048e5d032a3c956623cf37eaf5
yaunga@hotmail.com <yaunga@hotmail.com>
2023-02-01 23:31:10 +0000

    Initial commit

--- src/transactions/transaction_processor.js
+++ src/transactions/transaction_processor.js
@@ -1,6 +1,6 @@
-var txr = [];
+// var txr = [];
 
-function processTransactions(transActions) {
+const processTransactions = (transActions) => {
 
     txr = [];
 
@@ -8,14 +8,15 @@ function processTransactions(transActions) {
         throw new Error("Undefined collection of transactions")
     }
 
-    let txCount = {}
+    // let txCount = {}
 
-    const numberOfTransactions = transActions.length;
+    // const numberOfTransactions = transActions.length;
 
-    for(var i = 0; i < numberOfTransactions; i++) {
-        const transaction = transActions[i];
-        txCount[transaction] ? txCount[transaction] += 1 : txCount[transaction] = 1;
-    }
+    // for(var i = 0; i < numberOfTransactions; i++) {
+    //     const transaction = transActions[i];
+    //     txCount[transaction] ? txCount[transaction] += 1 : txCount[transaction] = 1;
+    // }
+    let txCount = transActions.reduce((cnt, cur) => (cnt[cur] = cnt[cur] + 1 || 1, cnt), {});
 
     txCount = sortByAmountThenName(txCount);
     
@@ -25,7 +26,7 @@ function processTransactions(transActions) {
     });
 
     return txr;
-}
+};
 
 function sortByAmountThenName(txCount) {
     let sortedKeys = Object.keys(txCount).sort(function sortingFunction(itemOne, itemTwo) {
@@ -42,11 +43,12 @@ function sortByAmountThenName(txCount) {
 
 
 function validateTransactions(transactions) {
-    if(transactions === undefined) {
-        return false;
-    } 
+    // if(transactions === undefined) {
+    //     return false;
+    // } 
 
-    return true;
+    // return true;
+    return transactions === undefined ? false : true;
 }
 
 module.exports = processTransactions;
