ce8018f6d28615970b20272806b9eaf68db816d8
yaunga@hotmail.com <yaunga@hotmail.com>
2023-02-03 11:19:43 +0000

    fixed sortByAmountThenName

--- src/transactions/transaction_processor.js
+++ src/transactions/transaction_processor.js
@@ -17,28 +17,35 @@ const processTransactions = (transActions) => {
     //     txCount[transaction] ? txCount[transaction] += 1 : txCount[transaction] = 1;
     // }
     let txCount = transActions.reduce((cnt, cur) => (cnt[cur] = cnt[cur] + 1 || 1, cnt), {});
-
+    // console.log(txCount);
     txCount = sortByAmountThenName(txCount);
     
     // Place them back in array for returning
+    // Object.keys(txCount).forEach(function (key, index) {
+    //     txr[index] = `${key} ${txCount[key]}`;
+    // });
+
     Object.keys(txCount).forEach(function (key, index) {
-        txr[index] = `${key} ${txCount[key]}`;
+        txr[index] = `${txCount[key]}`.replace(",", " ");
     });
 
     return txr;
 };
 
 function sortByAmountThenName(txCount) {
-    let sortedKeys = Object.keys(txCount).sort(function sortingFunction(itemOne, itemTwo) {
-        return  txCount[itemTwo] - txCount[itemOne] || itemOne > itemTwo || -(itemOne < itemTwo)}
-    );
+    // let sortedKeys = Object.keys(txCount).sort(function sortingFunction(itemOne, itemTwo) {
+    //     return  txCount[itemTwo] - txCount[itemOne] || itemOne > itemTwo || -(itemOne < itemTwo)}
+    // );
+    // let sortedResults = {};
+    // for(let objectKey of sortedKeys) {
+    //     sortedResults[objectKey] = txCount[objectKey];
+    // }
 
-    let sortedResults = {};
-    for(let objectKey of sortedKeys) {
-        sortedResults[objectKey] = txCount[objectKey];
-    }
+    // return sortedResults;
+    keysSorted = Object.entries(txCount).sort((a,b)=>{return b[1] - a[1] || a > b || -(a < b)})
+    // console.log(keysSorted);    
 
-    return sortedResults;
+    return keysSorted;
 }
 
 
